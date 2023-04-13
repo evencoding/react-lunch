34fb032123d249dd7b41404fba60a29e97fc5ee1
Jungwoo <evencoding@gmail.com>
2023-04-13 14:44:46 +0900

    Updates

---
+++ precache-manifest.60ece8a72280f19bea8a60efa96d591d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8af183d59ab1ade19fba",
+    "url": "/react-lunch/static/js/main.61e0a00b.chunk.js"
+  },
+  {
+    "revision": "87a70218c3390b341ecf",
+    "url": "/react-lunch/static/js/runtime~main.016e3eae.js"
+  },
+  {
+    "revision": "5bdb421aa172583d43e6",
+    "url": "/react-lunch/static/js/2.99021d9f.chunk.js"
+  },
+  {
+    "revision": "8a0ec07ec3b3ee95ee7eca5967e82dc5",
+    "url": "/react-lunch/index.html"
+  }
+];
